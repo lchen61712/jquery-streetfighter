9c7dff9695cbf1f4de81b4d5a287fb16eadca564
Lily Chen <lchen61712@gmail.com>
2016-06-29 16:30:33 -0700

    commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+	var hover = false
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		hover = true;
 	})
 	.mouseleave(function() {
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+		hover = false;
 	})
 	.mousedown(function() {
 		playHadouken();
@@ -22,11 +25,29 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	});
+	})
+	$(document).keydown(function(e) {
+		if(e.which == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	$(document).keyup(function(e) {
+		if(e.which == 88) {
+			$('.ryu-cool').hide();
+			if (hover == false) {
+				$('.ryu-still').show();
+			}
+			else {
+				$('.ryu-ready').show();
+			}
+		}
+	})
 });
 
 function playHadouken (){
-	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].volume = 0.25;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
